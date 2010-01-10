789370d37526ad8402184abcc50613e6ebb7d9d7
Jan Hühne <jan.huehne@weimar.eins.de>
2010-01-11 00:07:45 +0100

    Umstellung auch Pool_Tasks für die Echos.

--- framework/ada_mr-master-server.ads
+++ framework/ada_mr-master-server.ads
@@ -28,21 +28,12 @@ package Ada_Mr.Master.Server is
   package ASU renames Ada.Strings.Unbounded;
   
   function Exit_Server return Boolean;
-  procedure Process_Incomming_Connection(New_Sock : Socket_Type);
   procedure Process_Request(S : Stream_Access; From : Ada_Mr.Helper.Worker_Type; Xml_Root : Ada_Mr.Xml.Node_Access);
   
   package Server is new Ada_Mr.Generics.Server(
     Exit_Server,
-    Process_Incomming_Connection,
+    Process_Request,
     Stop_Master
   );
-  
-  package Echo_MR is new Ada_Mr.Generics.Echo(
-    Process_Request
-  );
-  
-  
-  
-  
   
 end Ada_Mr.Master.Server;
