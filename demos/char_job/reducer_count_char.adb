2f5e8c320b5e3b070c3cff6af43ba9f62f906cad
Jan Hühne <jan.huehne@weimar.eins.de>
2010-01-18 12:16:14 +0100

    Nun auch mit den Startdateien.

---
+++ demos/char_job/reducer_count_char.adb
@@ -0,0 +1,22 @@
+with Ada.Command_Line;
+with Char_Job;
+with Ada_Mr.Reducer.Main;
+
+
+procedure Reducer_Count_Char is
+  package Job renames Char_Job;
+  
+  package Reducer_MR is new Ada_Mr.Reducer.Main(
+    Job.Merge_Job_Results, 
+    Char_Job.Finalize
+  );
+  
+begin
+  
+  declare
+    C : Reducer_MR.Reducer_Task_Access := new Reducer_MR.Reducer_Task;
+  begin
+    C.Start(C);
+  end;
+  
+end Reducer_Count_Char;
