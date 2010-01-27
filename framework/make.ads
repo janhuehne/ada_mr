0bb7bbcd1149a78ea0f5dc0ab5069e33f669005f
Jan Hühne <jan.huehne@weimar.eins.de>
2010-01-27 19:25:49 +0100

    Baut das Framework

--- framework/make.ads
+++ framework/make.ads
@@ -1,27 +1,38 @@
+with Ada_Mr;
 with Ada_Mr.Helper;
+
 with Ada_Mr.Crypt.Helper;
+
 with Ada_Mr.Generics.Console;
 with Ada_Mr.Generics.Echo;
-with Ada_Mr.Generics.Observer;
 with Ada_Mr.Generics.Runner;
 with Ada_Mr.Generics.Server;
+
 with Ada_Mr.Logger;
+
 with Simple_Xml;
-with Simple_Ada_Mr.Xml.Xml_Io;
+with Simple_Xml.Xml_Io;
+
 with Ada_Mr.Xml;
 with Ada_Mr.Xml.Helper;
 with Ada_Mr.Xml.Parser;
 
 -- Master;
+with Ada_Mr.Master;
 with Ada_Mr.Master.Helper;
 with Ada_Mr.Master.Server;
+with Ada_Mr.Master.Main;
 
 -- Mapper
+with Ada_Mr.Mapper;
+with Ada_Mr.Mapper.Main;
 with Ada_Mr.Mapper.Helper;
 with Ada_Mr.Mapper.Runner;
 with Ada_Mr.Mapper.Server;
 
 -- Reducer
+with Ada_Mr.Reducer;
+with Ada_Mr.Reducer.Main;
 with Ada_Mr.Reducer.Helper;
 with Ada_Mr.Reducer.Runner;
 with Ada_Mr.Reducer.Server;
