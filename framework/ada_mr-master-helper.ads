32a2f83f40af8d3e0109771544060bafb729df78
Jan Hühne <jahu@Jan-Huhnes-MacBook-Pro.local>
2010-02-22 16:44:36 +0100

    Dokumentation

--- framework/ada_mr-master-helper.ads
+++ framework/ada_mr-master-helper.ads
@@ -1,3 +1,42 @@
+-------------------------------------------------------------------------------
+-- <STRONG>Copyright &copy; 2009, 2010 by Jan-Hendrik Hühne.</STRONG>
+-- <BLOCKQUOTE>
+--   This program is free software; you can redistribute it and/or
+--   modify it under the terms of the GNU General Public License as
+--   published by the Free Software Foundation; either version 2 of the
+--   License, or (at your option) any later version.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   This program is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--   General Public License for more details.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   You should have received a copy of the GNU General Public License
+--   along with this program; if not, write to the Free Software
+--   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+--   02111-1307, USA.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   As a special exception, if other files instantiate generics from
+--   this unit, or you link this unit with other files to produce an
+--   executable, this unit does not by itself cause the resulting
+--   executable to be covered by the GNU General Public License. This
+--   exception does not however invalidate any other reasons why the
+--   executable file might be covered by the GNU Public License.
+-- </BLOCKQUOTE>
+--
+--  <AUTHOR>
+--    Jan-Hendrik Hühne <E_MAIL>
+--  </AUTHOR>
+--
+--  <PURPOSE>
+--    Helper package for the Ada_Mr.Master component. Provides a couple 
+--    of helper methods.
+--  </PURPOSE>
+-------------------------------------------------------------------------------
+
 with Ada.Containers.Vectors;
 with Ada.Strings.Unbounded;
 with Ada_Mr.Xml;
@@ -8,31 +47,28 @@ with Ada.Calendar;
 
 package Ada_Mr.Master.Helper is
 
-----------------------------------------------------
--- PACkAGE RENAMES                                 -
-----------------------------------------------------
   package ASU renames Ada.Strings.Unbounded;
   
   
   
-----------------------------------------------------
--- TYPE DEFINITIONS AND FUNCTIONS                  -
-----------------------------------------------------
+  
   type Job_State is (
     Pending,
     In_Progress,
     Done,
     Failed
   );
+  -- Definition for different job states
   
   function To_String(Arg : Job_State) return String;
+  -- Returns a job state as a string
+  
   function From_String(Arg : String) return Job_State;
+  -- Returns a job state from a string
   
   
   
-----------------------------------------------------
--- WORKER RECORD                                   -
-----------------------------------------------------
+  
   type Worker_Record is record
     Identifier   : ASU.Unbounded_String;
     W_Type       : Ada_Mr.Helper.Worker_Type;
@@ -41,49 +77,63 @@ package Ada_Mr.Master.Helper is
     Access_Token : String(1..32);
     Updated_At   : Ada.Calendar.Time;
   end record;
+  -- Record that stores a worker with the metadata
   
   type Worker_Record_Access is access Worker_Record;
+  -- Access type for a Worker_Record
   
   package Worker_Entry_Vectors is new Ada.Containers.Vectors(
     Element_Type => Worker_Record_Access,
     Index_Type => Positive
   );
+  -- Vector that stores the connected worker
   
   
   
-----------------------------------------------------
--- NOT DELIVERED MAP RESULT                         -
-----------------------------------------------------
+  
   type Not_Delivered_Map_Result is record
     Reducer   : ASU.Unbounded_String;
     Result    : ASU.Unbounded_String;
   end record;
+  -- Record that stores not delivered mapper results
   
   type Not_Delivered_Map_Result_Access is access Not_Delivered_Map_Result;
+  -- Access type for a Not_Delivered_Map_Result
   
   package Not_Delivered_Map_Result_Vectors is new Ada.Containers.Vectors(
     Element_Type => Not_Delivered_Map_Result_Access,
     Index_Type => Positive
   );
+  -- Vector that stores the not delivered mapper results
   
   protected Not_Delivered_Map_Results is
     procedure Add(Reducer : String; Result : String);
+    -- Procedure to add a new not delivered mapper result
+      
     function Get_All_By_Identifier(Identifier : String) return Not_Delivered_Map_Result_Vectors.Vector;
+    -- Returns a vector with all not delivered mapper results for a given worker
+      
     function Is_Empty return Boolean;
+    -- Returns true if no not delivered mapper results available
   end;
   
   Not_Delivered_Map_Results_Vector : Not_Delivered_Map_Result_Vectors.Vector;
   
   
   
-----------------------------------------------------
--- PROTECTED TYPE TO HANDLE ABORT  MESSAGES        -
-----------------------------------------------------
+  
   protected Aborted is
     procedure Set_Abort;
+    -- Procedure to abort the complete system
+    
     procedure Set_Exit;
+    -- Procedure to exit the complete system
+      
     function Get_Abort return Boolean;
+    -- Returns the abort status
+    
     function Get_Exit return Boolean;
+    -- Returns the exit status
   private
     Abort_Master  : Boolean := false;
     Exit_Master   : Boolean := false;
@@ -91,9 +141,7 @@ package Ada_Mr.Master.Helper is
   
   
   
-----------------------------------------------------
--- EXCEPTION DEFINITIONS                           -
-----------------------------------------------------
+  
   No_Job_Found : Exception;
   Initialization_Required : Exception;
   No_Worker_Found : Exception;
