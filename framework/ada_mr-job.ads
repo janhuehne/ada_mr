486dee63116f7b3f79415c7f1c262ec6bb26b525
Jan Hühne <jahu@Jan-Huhnes-MacBook-Pro.local>
2010-02-24 17:03:17 +0100

    Dokumentation

--- framework/ada_mr-job.ads
+++ framework/ada_mr-job.ads
@@ -1,52 +1,105 @@
+-------------------------------------------------------------------------------
+-- <STRONG>Copyright &copy; 2009, 2010 by Jan-Hendrik H&uuml;hne.</STRONG>
+-- <BLOCKQUOTE>
+--   This program is free software; you can redistribute it and/or
+--   modify it under the terms of the GNU General Public License as
+--   published by the Free Software Foundation; either version 2 of the
+--   License, or (at your option) any later version.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   This program is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--   General Public License for more details.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   You should have received a copy of the GNU General Public License
+--   along with this program; if not, write to the Free Software
+--   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+--   02111-1307, USA.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   As a special exception, if other files instantiate generics from
+--   this unit, or you link this unit with other files to produce an
+--   executable, this unit does not by itself cause the resulting
+--   executable to be covered by the GNU General Public License. This
+--   exception does not however invalidate any other reasons why the
+--   executable file might be covered by the GNU Public License.
+-- </BLOCKQUOTE>
+--
+--  <AUTHOR>
+--    Bauhaus-University Weimar<br />
+--    Jan-Hendrik H&uuml;hne <jan.huehne@uni-weimar.de>
+--  </AUTHOR>
+--
+--  <PURPOSE>
+--    Abstract job package.
+--  </PURPOSE>
+-------------------------------------------------------------------------------
+
 with Ada_Mr.Xml;
 with Ada_Mr.Helper;
 
 package Ada_Mr.Job is
   
-  -- record
   type Object is abstract tagged record
     Job_Id : Natural := 0;
   end record;
   
+  function To_Xml
+    (The_Job : Object) 
+    return String is abstract;
   -- Serializes a job into the xml format
-  function To_Xml(The_Job : Object) return String is abstract;
   
+  function From_Xml
+    (Xml_Node : Ada_Mr.Xml.Node_Access)
+    return Object is abstract;
   -- Deserializes a job from the xml format
-  function From_Xml(Xml_Node : Ada_Mr.Xml.Node_Access) return Object is abstract;
   
+  function Get_Job_Id
+    (The_Job : Object)
+    return Natural;
   --  Returns the id from a given job
-  function Get_Job_Id(The_Job : Object) return Natural;
   
+  procedure Split_Raw_Data 
+    is abstract;
   -- Splits the raw data into comoputable subjobs
-  procedure Split_Raw_Data is abstract;
   
+  function Get_Next_Raw_Job 
+    return Object is abstract;
   -- Is called from the generic master to import the jobs
-  function Get_Next_Raw_Job return Object is abstract;
   
-  -- Called by the generic system to print job in stio
-  procedure Print_Job(The_Job : Object; State : String; Message : String);
+  procedure Print_Job
+    (The_Job : Object; 
+     State   : String; 
+     Message : String);
+  -- Called by the generic system to print job in STD/IO
   
+  procedure Compute_Job
+    (The_Job : Object) is abstract;
   -- Called by the generic mapper to compute a job
-  procedure Compute_Job(The_Job : Object) is abstract;
   
+  function Split_Result_For_Different_Reducer 
+    return Ada_Mr.Helper.String_String_Maps.Map is abstract;
   -- Function to split a result for serval reducers
-  function Split_Result_For_Different_Reducer return Ada_Mr.Helper.String_String_Maps.Map is abstract;
   
+  procedure Merge_Job_Results
+    (Xml_Node    : Ada_Mr.Xml.Node_Access; 
+     Stop_System : out Boolean) is abstract;
   -- Procedure to merge pending job results
-  procedure Merge_Job_Results(Xml_Node : Ada_Mr.Xml.Node_Access; Stop_System : out Boolean) is abstract;
   
+  procedure Finalize 
+    is abstract;
   -- Procedure called by the reducer to handle the job result
-  procedure Finalize is abstract;
   
+  function Get_Next_Job_Id 
+    return Natural;
+  -- Returns the next job id
   
-  function Get_Next_Job_Id return Natural;
-  
-  procedure Set_Job_Id(The_Job : in out Object);
+  procedure Set_Job_Id
+    (The_Job : in out Object);
+  -- Set for the <code>job_id</code>
 
 private
-  
-  -- Counts the jobs
   Job_Counter : Natural := 1;
-
-
 end Ada_Mr.Job;
--- framework/ada_mr-logger.ads
+++ framework/ada_mr-logger.ads
@@ -1,27 +1,118 @@
+-------------------------------------------------------------------------------
+-- <STRONG>Copyright &copy; 2009, 2010 by Jan-Hendrik H&uuml;hne.</STRONG>
+-- <BLOCKQUOTE>
+--   This program is free software; you can redistribute it and/or
+--   modify it under the terms of the GNU General Public License as
+--   published by the Free Software Foundation; either version 2 of the
+--   License, or (at your option) any later version.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   This program is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--   General Public License for more details.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   You should have received a copy of the GNU General Public License
+--   along with this program; if not, write to the Free Software
+--   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+--   02111-1307, USA.
+-- </BLOCKQUOTE>
+-- <BLOCKQUOTE>
+--   As a special exception, if other files instantiate generics from
+--   this unit, or you link this unit with other files to produce an
+--   executable, this unit does not by itself cause the resulting
+--   executable to be covered by the GNU General Public License. This
+--   exception does not however invalidate any other reasons why the
+--   executable file might be covered by the GNU Public License.
+-- </BLOCKQUOTE>
+--
+--  <AUTHOR>
+--    Bauhaus-University Weimar<br />
+--    Jan-Hendrik H&uuml;hne <jan.huehne@uni-weimar.de>
+--  </AUTHOR>
+--
+--  <PURPOSE>
+--    A logger system.
+--  </PURPOSE>
+-------------------------------------------------------------------------------
+
 package Ada_Mr.Logger is
   
-  type Log_Level is (Info, Warn, Err, User, System, Debug);
+  type Log_Level is 
+    (Info, 
+     Warn, 
+     Err, 
+     User, 
+     System, 
+     Debug);
   
-  procedure Set_Output_Level(Level : Log_Level);
-  procedure Set_Output_Level(Level : String);
+  procedure Set_Output_Level
+    (Level : Log_Level);
+  -- Sets the output level
   
-  procedure Put(Item : String; Level : Log_Level; Prefix : String := "");
-  procedure Put_Line(Item : String; Level : Log_Level; Prefix : String := "");
-  procedure New_Line(Level : Log_Level);
+  procedure Set_Output_Level
+    (Level : String);
+  -- Sets the output level
   
-  procedure Info(Item : String; Prefix : String := "");
-  procedure Warn(Item : String; Prefix : String := "");
-  procedure Error(Item : String; Prefix : String := "");
-  procedure User(Item : String; Prefix : String := "");
-  procedure System(Item : String; Prefix : String := "");
-  procedure Debug(Item : String; Prefix : String := "");
+  procedure Put
+    (Item   : String;
+     Level  : Log_Level;
+     Prefix : String := "");
+  -- Puts the <code>Item</code> on STD/IO with additions log informations.
+  
+  procedure Put_Line
+    (Item   : String;
+     Level  : Log_Level; 
+     Prefix : String := "");
+  -- Puts the <code>Item</code> on STD/IO with additions log informations.
+  
+  procedure New_Line
+    (Level : Log_Level);
+  -- Puts a new line on STD/IO with the log level.
+  
+  procedure Info
+    (Item : String;
+     Prefix : String := "");
+  -- Wrapper for <code>Put_Line(Item, Info, Prefix);</code>.
+  
+  procedure Warn
+    (Item   : String;
+     Prefix : String := "");
+  -- Wrapper for <code>Put_Line(Item, Warn, Prefix);</code>.
+  
+  procedure Error
+    (Item   : String;
+     Prefix : String := "");
+  -- Wrapper for <code>Put_Line(Item, Error, Prefix);</code>.
+    
+  procedure User
+    (Item : String;
+     Prefix : String := "");
+  -- Wrapper for <code>Put_Line(Item, User, Prefix);</code>
+    
+  procedure System
+    (Item : String;
+     Prefix : String := "");
+  -- Wrapper for <code>Put_Line(Item, System, Prefix);</code>
+    
+  procedure Debug
+    (Item : String;
+     Prefix : String := "");
+  -- Wrapper for <code>Put_Line(Item, Debug, Prefix);</code>
   
   function Image(Level : Log_Level) return String;
+  -- Returns the given log level as a string
+  
   function From_String(Level : String) return Log_Level;
+  -- Converts a string to the log level
   
 private
   function Now return String;
+  -- Returns the current time
+  
   function Has_Correct_Level(Level : Log_Level) return Boolean;
+  -- Returns <code>True</code> if an output should occur.
   
   Output_Level : Log_Level := Info;
 end Ada_Mr.Logger;
